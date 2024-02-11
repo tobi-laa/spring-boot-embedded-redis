b124fe22efe6f80cdf11ab3dda7dc2996a49fb34
Tobias Laatsch <tobias.laatsch@posteo.de>
2024-02-11 20:00:10 +0100

    Adjust Gradle build and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.22"
     id("com.adarshr.test-logger") version "4.0.0"
@@ -16,6 +15,12 @@ java {
 
 repositories {
     mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.2")
+    }
 }
 
 dependencies {
