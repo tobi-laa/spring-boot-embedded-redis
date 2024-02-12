848d2f15d101537cc5b83b7172c96fe92ac6410e
Tobias Laatsch <tobias.laatsch@posteo.de>
2024-02-12 05:34:23 +0100

    Sonatype URLs

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,8 +135,8 @@ publishing {
     repositories {
         maven {
             name = "ossrh"
-            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
             credentials {
                 username = System.getenv("OSSRH_USERNAME")
