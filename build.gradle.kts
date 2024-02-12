76edfd4603e8d3e274b45ed5b3265ddf65bc88b1
Tobias Laatsch <tobias.laatsch@posteo.de>
2024-02-12 05:15:38 +0100

    GH workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,10 +147,9 @@ publishing {
 }
 
 signing {
-    val signingKeyId: String? by project
     val signingKey: String? by project
     val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["mavenJava"])
 }
 
