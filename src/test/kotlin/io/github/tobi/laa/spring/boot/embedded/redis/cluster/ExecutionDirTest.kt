b887f3b5b86bf9ee48a0c1152fb5014bd0d44642
Tobias Laatsch <tobias.laatsch@posteo.de>
2024-03-08 02:12:00 +0100

    :green_heart: Fix pipeline on Windows

--- src/test/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/cluster/ExecutionDirTest.kt
+++ src/test/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/cluster/ExecutionDirTest.kt
@@ -2,17 +2,29 @@ package io.github.tobi.laa.spring.boot.embedded.redis.cluster
 
 import io.github.tobi.laa.spring.boot.embedded.redis.IntegrationTest
 import io.github.tobi.laa.spring.boot.embedded.redis.RedisTests
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.io.File
+
+private const val CUSTOM_EXECUTION_DIR = "build/custom-execution-dir-cluster"
 
 @IntegrationTest
-@EmbeddedRedisCluster(executeInDirectory = "build")
+@EmbeddedRedisCluster(executeInDirectory = CUSTOM_EXECUTION_DIR)
 @DisplayName("Using @EmbeddedRedisCluster with custom execution dir")
 internal class ExecutionDirTest {
 
     @Autowired
     private lateinit var given: RedisTests
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            File(CUSTOM_EXECUTION_DIR).mkdirs()
+        }
+    }
 
     @Test
     @DisplayName("It should be possible to write to Redis and the data should be available afterwards")
--- src/test/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/highavailability/ExecutionDirTest.kt
+++ src/test/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/highavailability/ExecutionDirTest.kt
@@ -2,17 +2,29 @@ package io.github.tobi.laa.spring.boot.embedded.redis.highavailability
 
 import io.github.tobi.laa.spring.boot.embedded.redis.IntegrationTest
 import io.github.tobi.laa.spring.boot.embedded.redis.RedisTests
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.io.File
+
+private const val CUSTOM_EXECUTION_DIR = "build/custom-execution-dir-high-availability"
 
 @IntegrationTest
-@EmbeddedRedisHighAvailability(executeInDirectory = "build")
+@EmbeddedRedisHighAvailability(executeInDirectory = CUSTOM_EXECUTION_DIR)
 @DisplayName("Using @EmbeddedRedisHighAvailability with custom execution dir")
 internal class ExecutionDirTest {
 
     @Autowired
     private lateinit var given: RedisTests
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            File(CUSTOM_EXECUTION_DIR).mkdirs()
+        }
+    }
 
     @Test
     @DisplayName("It should be possible to write to Redis and the data should be available afterwards")
