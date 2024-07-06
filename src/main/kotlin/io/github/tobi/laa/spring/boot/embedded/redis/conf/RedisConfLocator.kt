be9e2612266c56f2aed5ba07b22796d169ddd168
Tobias Laatsch <tobias.laatsch@posteo.de>
2024-07-06 11:27:46 +0200

    :white_check_mark: Increase coverage

--- src/main/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/conf/RedisConfLocator.kt
+++ src/main/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/conf/RedisConfLocator.kt
@@ -18,7 +18,7 @@ internal object RedisConfLocator {
     @Suppress("UNCHECKED_CAST")
     internal fun locate(server: Redis): Path {
         val args = ARGS_PROP[server as RedisInstance] as List<String>
-        return args.find { it.endsWith(".conf") }?.let { Paths.get(it) }
-            ?: throw IllegalStateException("No config file found for embedded Redis server: $server")
+        val redisConf = args.find { it.endsWith(".conf") }?.let { Paths.get(it) }
+        return redisConf ?: throw IllegalStateException("No config file found for embedded Redis server: $server")
     }
 }
--- src/test/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/conf/RedisConfLocatorTest.kt
+++ src/test/kotlin/io/github/tobi/laa/spring/boot/embedded/redis/conf/RedisConfLocatorTest.kt
@@ -1,11 +1,13 @@
 package io.github.tobi.laa.spring.boot.embedded.redis.conf
 
 import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import redis.embedded.RedisInstance
+import java.nio.file.Paths
 
 @DisplayName("Tests for RedisConfLocator")
 @ExtendWith(MockKExtension::class)
@@ -27,6 +29,13 @@ internal class RedisConfLocatorTest {
         assertThatThrownBy { RedisConfLocator.locate(redis) }
             .isExactlyInstanceOf(IllegalStateException::class.java)
             .hasMessage("No config file found for embedded Redis server: $redis")
+    }
+
+    @Test
+    @DisplayName("RedisConfLocator should return path to redis.conf file if it exists")
+    fun redisConfFound_shouldReturnPath() {
+        val redis = TestRedis(listOf("redis.conf"))
+        assertThat(RedisConfLocator.locate(redis)).isEqualTo(Paths.get("redis.conf"))
     }
 
     private class TestRedis(args: List<String>) : RedisInstance(0, args, null, false, null, null) {
